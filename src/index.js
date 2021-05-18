87fcda61ba24f2b70cda00759235d450964f03aa
Maryna-Skrypnyk <marina.skrypnyk@gmail.com>
2021-05-18 18:51:18 +0300

    Start

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 import './sass/main.scss';
+
+// new CountdownTimer({
+//   selector: '#timer-1',
+//   targetDate: new Date('Jul 17, 2019'),
+// });
+
+const timer = {
+  start() {
+    const startTime = Date.now();
+
+    setInterval(() => {
+      const currentTime = new Date('2019, Jul 17');
+      const deltaTime = startTime - currentTime;
+      const { days, hours, mins, secs } = getTimeComponents(deltaTime);
+
+      console.log(`${days}:${hours}:${mins}:${secs}`);
+    }, 1000);
+  },
+};
+
+timer.start();
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
+
+function getTimeComponents(time) {
+  const days = Math.floor(time / (1000 * 60 * 60 * 24));
+  const hours = pad(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+  );
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+  return { days, hours, mins, secs };
+}
